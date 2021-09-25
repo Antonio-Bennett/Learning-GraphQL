f5cae11e551944ef374ab199f4ca77f47999e9dd
Antonio Bennett <bennettantonio199@gmail.com>
2021-09-24 23:43:45 -0400

    config port correctly

--- server/app.js
+++ server/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 
 require("dotenv").config();
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
@@ -26,4 +26,4 @@ app.use(
   })
 );
 
-const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
